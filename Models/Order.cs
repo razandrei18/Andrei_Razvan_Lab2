0bd83a0a2c94df595bf7de127b9dfa84aa3f65c9
razandrei18 <razvanioan.andrei22@gmail.com>
2020-10-25 19:57:40 +0200

    Laborator 3 adaugat - complet

--- Models/Order.cs
+++ Models/Order.cs
@@ -11,6 +11,7 @@ namespace Andrei_Razvan_Lab2.Models
         public int CustomerID { get; set; }
         public int BookID { get; set; }
 
+        public DateTime OrderDate { get; set; }
         public Customer Customer { get; set; }
         public Book Book { get; set; }
     }

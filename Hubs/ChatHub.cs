8976d8592e54ab080bbef8b125f47679caa60a40
razandrei18 <razvanioan.andrei22@gmail.com>
2020-12-03 18:04:11 +0200

    Laborator 9 complet

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +7,12 @@ using System.Threading.Tasks;
 
 namespace Andrei_Razvan_Lab2.Hubs
 {
+    [Authorize]
     public class ChatHub : Hub
     {
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", Context.User.Identity.Name, message);
         }
     }
 }
